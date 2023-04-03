ace2c7b4e9f1171cc632b558a7f0f2053154f580
Ibnu Habibie <bhentz94@gmail.com>
2023-04-03 13:48:50 +0700

    add block options

--- src/blocks.js
+++ src/blocks.js
@@ -1,10 +1,12 @@
 export default (editor, opts = {}) => {
   const bm = editor.BlockManager;
 
-  bm.add('bootstrapIcon', {
-    label: 'Bootstrap Icon',
-    media: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill="none" d="M0 0h16v16H0z"/><path fill="#F29900" d="M13.5 2H8L1 13h5.5z"/><path fill="#1A73E8" d="M8 2h5v11H8z"/><circle fill="#EA4335" cx="3.25" cy="4.25" r="2.25"/><path fill="#0D652D" d="M13.33 10L13 13c-1.66 0-3-1.34-3-3s1.34-3 3-3l.33 3z"/><path fill="#174EA6" d="M10.5 4.5A2.5 2.5 0 0113 2l.45 2.5L13 7a2.5 2.5 0 01-2.5-2.5z"/><path fill="#1A73E8" d="M13 2a2.5 2.5 0 010 5"/><path fill="#34A853" d="M13 7c1.66 0 3 1.34 3 3s-1.34 3-3 3"/></svg>`,
-    category: "Basic",
-    content: `<i data-gjs-type="bootstrapIcon"  class="bi-alarm" style="font-size: 20rem; color: cornflowerblue;"></i>`,
+  bm.add("bootstrapIcon", {
+    label: opts.label ?? "Bootstrap Icon",
+    media:
+      opts.media ??
+      `<svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" class="d-block my-1" viewBox="0 0 118 94" role="img"><title>Bootstrap</title><path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z" fill="currentColor"></path></svg>`,
+    category: opts.category ?? "Basic",
+    content: `<i data-gjs-type="bootstrapIcon" class="bi-alarm"></i>`,
   });
-}
+};
